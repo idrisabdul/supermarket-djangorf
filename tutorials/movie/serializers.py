dbf0178b951f61a0a068526a76db91105fdfb9aa
idrisabdul <idrisaziz52@gmail.com>
2024-08-13 10:51:35 +0700

    endpoint

--- tutorials/movie/serializers.py
+++ tutorials/movie/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Movie
 
-class MovieSerializer(serializers.MovieSerializer):
+class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
