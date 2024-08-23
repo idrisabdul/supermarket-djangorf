e7748a5b706a835efc48b9aea9404763de79fea3
idrisabdul <idrisaziz52@gmail.com>
2024-08-23 09:12:37 +0700

    test

--- tutorials/movie/urls.py
+++ tutorials/movie/urls.py
@@ -3,7 +3,7 @@ from .views import MovieListCreateView, MovieDetailView, AllMoviesListView, Movi
 
 urlpatterns = [
     path('movies/', MovieListCreateView.as_view(), name='movie-list-create'),
-    path('movies/<int:pk>', MovieDetailView.as_view(), name='movie-detail'),
+    path('movie/<int:pk>', MovieDetailView.as_view(), name='movie-detail'),
     path('movies/all', AllMoviesListView.as_view(), name='all-movies-list'),
     path('movies/delete/<int:pk>', MovieDeleteView.as_view(), name='movie-delete'),
     path('movies/update/<int:pk>', MovieUpdateView.as_view(), name='movie-update'),
